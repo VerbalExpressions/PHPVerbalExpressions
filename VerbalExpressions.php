d636108513392f88cfc073340975c32f46078845
William Knauss <will.knauss@gmail.com>
2013-08-10 12:55:19 -0400

    basic cleanup

--- VerbalExpressions.php
+++ VerbalExpressions.php
@@ -40,51 +40,86 @@
 
 class VerEx {
 
-	var $prefixes = "";
-	var $source = "";
-	var $suffixes = "";
-	var $modifiers = "m"; // default to global multiline matching
-	var $replaceLimit = 1; // the limit of preg_replace when g modifier is not set
+	var $prefixes     = "";
+	var $source       = "";
+	var $suffixes     = "";
+	var $modifiers    = "m"; // default to global multiline matching
+	var $replaceLimit = 1;   // the limit of preg_replace when g modifier is not set
 
 	/**
+	 * Sanitize
+	 *
 	 * Sanitation function for adding anything safely to the expression
+	 *
+	 * @access public
 	 * @param  string $value the to be added
 	 * @return string        escaped value
 	 */
-	function sanitize( $value ) {
+	public function sanitize($value) 
+	{
 		if(!$value) 
+		{
 			return $value;
+		}
 		return preg_quote($value, "/");
 	}
+
 	/**
+	 * Add
+	 *
 	 * Add stuff to the expression 
+	 *
+	 * @access public
 	 * @param string $value the stuff to be added
+	 * @return object
 	 */
-	function add( $value ) {
+	public function add($value) 
+	{
 		$this->source .= $value;
 		return $this;
 	}
+
 	/**
+	 * Start of Line
+	 *
 	 * Mark the expression to start at the beginning of the line.
+	 *
+	 * @access public
 	 * @param  boolean $enable Enables or disables the line starting. Default value: true
+	 * @return object
 	 */
-	function startOfLine( $enable = true ) {
+	public function startOfLine($enable=true)
+	{
 		$this->prefixes = $enable ? "^" : "";
 		return $this;
 	}
+
 	/**
+	 * End of line
+	 *
 	 * Mark the expression to end at the last character of the line.
+	 *
+	 * @access public
 	 * @param  boolean $enable Enables or disables the line ending. Default value: true
+	 * @return object
 	 */
-	function endOfLine( $enable = true ) {
+	public function endOfLine($enable=true) 
+	{
 		$this->suffixes = $enable ? "$" : "";
 		return $this;
 	}
+
 	/**
+	 * Add
+	 *
 	 * Add a string to the expression
+	 *
+	 * @access public
 	 * @param  string $value The string to be looked for
+	 * @return object
 	 */
-	function then( $value ) {
+	public function then($value) 
+	{
 		$this->add("(".$this->sanitize($value).")");
 		return $this;
 	}
@@ -93,185 +128,341 @@ class VerEx {
 	 * alias for then()
 	 * @param  string $value The string to be looked for
 	 */
-	function find( $value ) {
+	public function find($value) 
+	{
 		return $this->then($value);
 	}
+
 	/**
+	 * Maybe
+	 *
 	 *  Add a string to the expression that might appear once (or not).
+	 *
+	 * @access public
 	 * @param  string $value The string to be looked for
+	 * @return object
 	 */
-	function maybe( $value ) {
+	public function maybe($value) 
+	{
 		$this->add("(".$this->sanitize($value).")?");
 		return $this;
 	}
+
 	/**
-	 * Accept any string 
+	 * Anything
+	 *
+	 * Accept any string
+	 *
+	 * @access public
+	 * @return object 
 	 */
-	function anything() {
+	public function anything() 
+	{
 		$this->add("(.*)");
 		return $this;
 	}
+
 	/**
+	 * Anthing But
+	 *
 	 * Anything but this chars
+	 *
+	 * @access public
 	 * @param  string $value The unaccepted chars
+	 * @return object
 	 */
-	function anythingBut( $value ) {
+	public function anythingBut( $value ) {
 		$this->add("([^". $this->sanitize($value) ."]*)");
 		return $this;
 	}
 
 	/**
+	 * Something
+	 *
 	 * Accept any non-empty string 
+	 *
+	 * @access public
+	 * @return object
 	 */
-	function something() {
+	public function something() 
+	{
 		$this->add("(.+)");
 		return $this;
 	}
 
 	/**
+	 * Anything but
+	 *
 	 * Anything non-empty except for these chars
+	 *
+	 * @access public
 	 * @param  string $value The unaccepted chars
+	 * @return object
 	 */
-	function somethingBut( $value ) {
+	public function somethingBut($value) 
+	{
 		$this->add("([^". $this->sanitize($value) ."]+)");
 		return $this;
 	}
 
 	/**
+	 * Preg Replace
+	 *
 	 * Shorthand for preg_replace()
+	 *
+	 * @access public
 	 * @param  string $source the string that will be affected(subject)
 	 * @param  string $value  the replacement
+	 * @return object
 	 */
-	function replace($source, $value) {
+	public function replace($source, $value) 
+	{
 		// php doesn't have g modifier so we remove it if it's there and we remove limit param
-		if(strpos($this->modifiers, 'g') !== false){
+		if(strpos($this->modifiers, 'g') !== false)
+		{
 			$this->modifiers = str_replace('g', '', $this->modifiers);
 			return preg_replace($this->getRegex(), $value, $source);
 		}		
+
 		return preg_replace($this->getRegex(), $value, $source, $this->replaceLimit);
 	}
+
 	/**
+	 * Linebreak
+	 *
 	 * Match line break
+	 *
+	 * @access public
+	 * @return object
 	 */
-	function lineBreak() {
+	public function lineBreak() 
+	{
 		$this->add("(\\n|(\\r\\n))");
 		return $this;
 	}
+
 	/**
+	 * Linebreak
+	 *
 	 * Shorthand for lineBreak
+	 *
+	 * @access public
+	 * return object
 	 */
-	function br() {
+	public function br() 
+	{
 		return $this->lineBreak();
 	}
+
 	/**
+	 * Tabs
+	 *
 	 * Match tabs.
+	 *
+	 * @access public
+	 * @return object
 	 */
-	function tab() {
+	public function tab() 
+	{
 		$this->add("\\t");
 		return $this;
 	}
+
 	/**
+	 * Alpha Numberic
+	 *
 	 * Match any alfanumeric
+	 *
+	 * @access public
+	 * @return object
 	 */
-	function word() {
+	public function word() 
+	{
 		$this->add("\\w+");
 		return $this;
 	}
+
 	/**
+	 * List Chars
+	 *
 	 * Any of the listed chars
+	 *
+	 * @access public
 	 * @param  string $value The chars looked for
+	 * @return object
 	 */
-	function anyOf( $value ) {
+	public function anyOf($value) 
+	{
 		$this->add("[". $value ."]");
 		return $this;
 	}
+
 	/**
+	 * Alias
+	 *
 	 * Shorthand for anyOf
+	 *
+	 * @access public
 	 * @param  string $value The chars looked for
+	 * @return object
 	 */
-	function any( $value ) {
+	public function any($value) 
+	{
 		return $this->anyOf($value);
 	}
+
 	/**
+	 * Add a range
+	 *
 	 * Adds a range to our expresion ex: range(a,z) => a-z, range(a,z,0,9) => a-z0-9
+	 *
+	 * @access public
+	 * @return object
 	 */
-	function range() {
+	public function range() 
+	{
 
 		$arg_num = func_num_args();
+
 		if($arg_num%2 != 0)
+		{
 			throw new Exception("Number of args must be even", 1);
+		}
+
 		$value = "[";
 		$arg_list = func_get_args();
+
 		for($i = 0; $i < $arg_num;)
+		{
 			$value .= $this->sanitize($arg_list[$i++]) . " - " . $this->sanitize($arg_list[$i++]);
+		}
+
 		$value .= "]";
 
 		$this->add($value);
 
 		return $this;
 	}
+
 	/**
+	 * Add a modifier
+	 *
 	 * Adds a modifier
+	 * 
+	 * @access public
+	 * @param str $modifier
+	 * @return object
 	 */
-	function addModifier( $modifier ) {
+	public function addModifier($modifier)
+	{
 		if(strpos($this->modifiers, $modifier) === false)
+		{
 			$this->modifiers .= $modifier;
+		}
 
 		return $this;
 	}
-	/**
-	 * Removes a modifier
-	 */
-	function removeModifier( $modifier ) {
 
+	/**
+	 * Remove Modifier
+	 *
+	 * Removes a modifier
+	 *
+	 * @access public
+	 * @param str $modifier
+	 * @return object
+	 */
+	public function removeModifier($modifier)
+	{
 		$this->modifiers = str_replace($modifier, '', $modifier);
 
 		return $this;
 	}
+
 	/**
+	 * Case Sensitivity
+	 *
 	 * Match case insensitive or sensitive based on $enable value
+	 *
+	 * @access public
 	 * @param  boolean $enable Enables or disables case sensitive. Default true
+	 * @return object
 	 */
-	function withAnyCase( $enable = true ) {
+	public function withAnyCase($enable=true)
+	{
 		if($enable)
+		{
 			$this->addModifier('i');
+		}
 		else
+		{
 			$this->removeModifier('i');
+		}
 
 		return $this;
 	}
+
 	/**
+	 * Stop At First
+	 *
 	 * Toggles g modifier
+	 *
+	 * @access public
 	 * @param  boolean $enable Enables or disables g modifier. Default true
+	 * @return object
 	 */
-	function stopAtFirst( $enable = true ) {
+	public function stopAtFirst($enable=true) 
+	{
 		if($enable)
+		{
 			$this->addModifier('g');
+		}
 		else
+		{
 			$this->removeModifier('g');
-		return $this;
-	}
-	/**
-	 * Toggles m modifier
-	 * @param  boolean $enable Enables or disables m modifier. Default true
-	 */
-	function searchOneLine( $enable = true ) {
-		if($enable)
-			$this->addModifier('m');
-		else
-			$this->removeModifier('m');
+		}
 
 		return $this;
 	}
+
 	/**
-	 * Adds the multiple modifier at the end of your expresion
-	 * @param  string $value Your expresion
+	 * SearchOneline
+	 *
+	 * Toggles m modifier
+	 *
+	 * @access public
+	 * @param  boolean $enable Enables or disables m modifier. Default true
+	 * @return object
 	 */
-	function multiple( $value ) {
-		
+	public function searchOneLine($enable=true) 
+	{
+		if($enable===true)
+		{
+			$this->addModifier('m');
+		}
+		else
+		{
+			$this->removeModifier('m');
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Multiple
+	 *
+	 * Adds the multiple modifier at the end of your expresion
+	 *
+	 * @access public
+	 * @param  string $value Your expresion
+	 * @return object
+	 */
+	public function multiple($value) 
+	{
 		$value = $this->sanitize($value);
 
-		switch (substr($value, -1)) {
+		switch (substr($value, -1))
+		{
 			case '+':
 			case '*':
 				break;
@@ -287,68 +478,103 @@ class VerEx {
 	}
 
 	/**
+	 * OR
+	 *
 	 * Wraps the current expresion in an `or` with $value
+	 *
+	 * @access public
 	 * @param  string $value new expression
+	 * @return object
 	 */
-	function _or( $value ) {
-		if(strpos($this->prefixes, "(") === false)
+	public function _or($value)
+	{
+		if(strpos($this->prefixes,"(")===false)
+		{
 			$this->prefixes .= "(";
-		if(strpos($this->suffixes, ")") === false)
+		}
+
+		if(strpos($this->suffixes, ")")===false)
+		{
 			$this->suffixes .= ")";
+		}
 
 		$this->add(")|(");
+
 		if($value)
+		{
 			$this->add($value);
+		}
 
 		return $this;
-
 	}
 
 	/**
+	 * Object to string
+	 *
 	 * PHP Magic method to return a string representation of the object.
 	 *
+	 * @access public
 	 * @return string
 	 */
-	public function __toString() {
+	public function __toString() 
+	{
 		return $this->getRegex();
 	}
 
 	/**
+	 * Get the Expression
+	 *
 	 * Creates the final regex.
+	 *
+	 * @access public
 	 * @return string The final regex
 	 */
-	function getRegex() {
-		return "/".$this->prefixes . $this->source . $this->suffixes. "/" . $this->modifiers;
+	public function getRegex() 
+	{
+		return "/".$this->prefixes.$this->source.$this->suffixes."/".$this->modifiers;
 	}
 
 	/**
+	 * Test
+	 *
 	 * tests the match of a string to the current regex
+	 *
+	 * @access public
 	 * @param  string $value The string to be tested
 	 * @return boolean        true if it's a match
 	 */
-	function test($value) {
+	public function test($value)
+	{
 		// php doesn't have g modifier so we remove it if it's there and call preg_match_all()
-		if(strpos($this->modifiers, 'g') !== false){
+		if(strpos($this->modifiers, 'g') !== false)
+		{
 			$this->modifiers = str_replace('g', '', $this->modifiers);
+
 			return preg_match_all($this->getRegex(), $value);
 		}
+
 		return preg_match($this->getRegex(), $value);
 	}
 
 	/**
+	 * Clean
+	 * 
 	 * deletes the current regex for a fresh start
+	 *
+	 * @access public
+	 * @param array $options
+	 * @return object
 	 */
-	function clean($options = array()) {
-		$options = array_merge(array("prefixes"=> "", "source"=>"", "suffixes"=>"", "modifiers"=>"gm","replaceLimit"=>"1"), $options);
-		$this->prefixes = $options['prefixes'];
-		$this->source = $options['source'];
-		$this->suffixes = $options['suffixes'];
-		$this->modifiers = $options['modifiers']; // default to global multiline matching
+	public function clean($options = array()) 
+	{
+		$options            = array_merge(array("prefixes"=> "", "source"=>"", "suffixes"=>"", "modifiers"=>"gm","replaceLimit"=>"1"), $options);
+		$this->prefixes     = $options['prefixes'];
+		$this->source       = $options['source'];
+		$this->suffixes     = $options['suffixes'];
+		$this->modifiers    = $options['modifiers'];    // default to global multiline matching
 		$this->replaceLimit = $options['replaceLimit']; // default to global multiline matching
 
 		return $this;
 	}
 
 }
-
-
