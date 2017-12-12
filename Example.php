b8c257700ac482a998b3ad9bd97391a28e5652f7
Björn Büttner <Idrinth@users.noreply.github.com>
2017-12-12 08:22:59 +0100

    making example and readme psr2 compliant (#27)
    moving squizlabs/php_codesniffer to dev, there's no need in prod

--- Example.php
+++ Example.php
@@ -10,57 +10,50 @@ $loader = require __DIR__ . '/vendor/autoload.php';
 $regex = new \VerbalExpressions\PHPVerbalExpressions\VerbalExpressions();
 
 $regex->startOfLine()
-	  ->then("http")
-	  ->maybe("s")
-	  ->then("://")
-	  ->maybe("www.")
-	  ->anythingBut(" ")
-	  ->endOfLine();
+      ->then("http")
+      ->maybe("s")
+      ->then("://")
+      ->maybe("www.")
+      ->anythingBut(" ")
+      ->endOfLine();
 
-if($regex->test("http://github.com"))
-	echo "valid url". '<br>';
-else
-	echo "invalid url". '<br>';
+if ($regex->test("http://github.com")) {
+    echo "valid url". '<br>';
+} else {
+    echo "invalid url". '<br>';
+}
 
 if (preg_match($regex, 'http://github.com')) {
-	echo 'valid url';
+    echo 'valid url';
 } else {
-	echo 'invalud url';
+    echo 'invalid url';
 }
 
 echo "<pre>". $regex->getRegex() ."</pre>";
 
 echo $regex->clean(array("modifiers" => "m", "replaceLimit" => 4))
-		   ->find(' ')
-		   ->replace("This is a small test http://somesite.com and some more text.", "-");
+           ->find(' ')
+           ->replace("This is a small test http://somesite.com and some more text.", "-");
 
 
 echo "<hr>";
 
 // limit
 echo $regex ->clean(array("modifiers" => "m"))
-			->startOfLine()
-			->anyOf("abc")
-			->anythingBut(" ")
-			->limit(1)
-			->withAnyCase()
-			->replace("Abracadabra is a nice word", "Ba");
+            ->startOfLine()
+            ->anyOf("abc")
+            ->anythingBut(" ")
+            ->limit(1)
+            ->withAnyCase()
+            ->replace("Abracadabra is a nice word", "Ba");
 
 echo "<br>";
 
 
 
 echo $regex ->clean(array("modifiers" => "gm"))
-			->add("\b")
-			->word()
-			->limit(2, 3)
-			->add("\b")
-			->replace("test abc ab abcd", "*");
-
-
-
-
-
-
-
-
+            ->add("\b")
+            ->word()
+            ->limit(2, 3)
+            ->add("\b")
+            ->replace("test abc ab abcd", "*");
